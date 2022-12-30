5f6817acf29e9af3e02eac18365e1926ccfb29b2
Udit <udi.pandey@gmail.com>
2022-12-30 09:24:33 +0530

    Level 1 - Username and Password Only

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.set('strictQuery', false);
+
+//connection to database
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+}
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+//home page
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+//login page
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+//register page
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+//adding new user
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+//log in the user
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+//start server
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
+});
