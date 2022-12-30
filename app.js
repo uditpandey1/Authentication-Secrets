d676dfe02d3f4c4a3ad152ef772ebf8cd4564ef4
udit pandey <31629633+uditpandey1@users.noreply.github.com>
2022-12-30 13:56:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets"
+  callbackURL: "https://secrets-site.cyclic.app/auth/google/secrets"
 },
 function(accessToken, refreshToken, profile, cb) {
   User.findOrCreate({ googleId: profile.id }, function (err, user) {
